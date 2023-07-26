85bf1000e15d64d76bf17dd39d6ffd80d1fbfed6
Fraser Davidson <fraser@frontierdigital.net>
2023-07-26 13:07:19 +0100

    WIP [major]

--- scripts/test.py
+++ scripts/test.py
@@ -1,30 +1,29 @@
+import os
 from python_terraform import Terraform
-
-
-def handle_error(return_code: int, stdout: str, stderr: str):
-    print(stdout)
-    print(stderr)
-    exit(return_code)
 
 
 def test_terraform(working_dir: str, validate: bool = True):
     print(f"testing terraform resources in '{working_dir}'")
 
-    t = Terraform(working_dir=working_dir)
-    return_code, stdout, stderr = t.fmt(check=True, diff=True)
+    terraform = Terraform(working_dir=working_dir)
+    return_code, _, _ = terraform.fmt(
+        check=True,
+        diff=True,
+        capture_output=False,
+    )
     if (return_code != 0):
-        handle_error(return_code, stdout, stderr)
+        exit(return_code)
 
     if (validate):
-        return_code, stdout, stderr = t.init(backend=False)
+        return_code, _, _ = terraform.init(backend=False, capture_output=False)
         if (return_code != 0):
-            handle_error(return_code, stdout, stderr)
+            exit(return_code)
 
-        return_code, stdout, stderr = t.validate()
+        return_code, _, _ = terraform.validate(capture_output=False)
         if (return_code != 0):
-            handle_error(return_code, stdout, stderr)
+            exit(return_code)
 
 
 if __name__ == "__main__":
-    test_terraform('src', False)
-    test_terraform('test')
+    test_terraform(os.path.join(os.getcwd(), "src"), False)
+    test_terraform(os.path.join(os.getcwd(), "test"))
